42923002138faea2296b328f013d3b5dba04c16d
Piotr Bogdan <ppbogdan@gmail.com>
2020-05-12 17:11:58 +0100

    nix, sources: update the update script

--- update.nix
+++ update.nix
@@ -1,17 +1,16 @@
-with  import (
-  fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarball/acbdaa569f4ee387386ebe1b9e60b9f95b4ab21b";
-    sha256 = "0xzyghyxk3hwhicgdbi8yv8b8ijy1rgdsj5wb26y5j322v96zlpz";
-  }
-) {};
 let
-  script = writeScriptBin "update.sh" ''
-    #!${pkgs.runtimeShell}
-
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.unstable {};
+  inherit (pkgs)
+    mkShell
+    writeShellScriptBin
+  ;
+  stack2nix = import sources.stack2nix {};
+  script = writeShellScriptBin "update.sh" ''
     rev=$1
 
-    for ghc in 8.6.5 8.8.2; do
-        ${pkgs.stack2nix}/bin/stack2nix \
+    for ghc in 8.6.5 8.8.2 8.8.3; do
+        ${stack2nix}/bin/stack2nix \
           --revision "$1" \
           --stack-yaml stack-"$ghc".yaml \
           https://github.com/haskell/haskell-ide-engine.git > hie/ghc-"$ghc".nix
